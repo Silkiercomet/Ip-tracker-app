218b45baceb023dfff1e456d7e549064485a7045
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-09-13 08:19:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-let btn = document.querySelector(".btn-input"),location2,timezone,isp1;
-let ip = "192.212.174.8";
-console.log(lat1)
+let btn = document.querySelector(".btn-input");
+
+const changeText = (x) => {
+    ip__show = document.getElementById("ip-getter").textContent = x.ip
+    loc = document.getElementById("location").textContent =`${x.location.city},${x.location.country}. ${x.location.postalCode}`
+    document.getElementById("timezone").textContent = x.location.timezone
+    document.getElementById("isf").textContent = x.isp
+}
+const ValidateIPaddress = (ipaddress) => {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+      return (true)  
+    }  
+
+  }  
+btn.addEventListener("click", api__getter)

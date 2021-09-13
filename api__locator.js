48273adf606de9472e8c797ccb8c70849f062018
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-09-13 08:19:56 -0400

    Update api__locator.js

--- api__locator.js
+++ api__locator.js
@@ -1,17 +1,30 @@
-var ip = "190.188.8.8";    
-    //var api_key = "at_gdyoltZTIMf5uDj0fRHL0HeN0Ovmv";    
-    var api_key = "at_gdyoltZTIMf5uDj0fRHL1HeN0Ovmv";    
-    $(function () {
-      $.ajax({
-          url: "https://geo.ipify.org/api/v1",
-          data: {apiKey: api_key, ipAddress: ip},
-          success: function(data) {
-               var x = data;
-               console.log(x.ip)
-               console.log(`${x.location.city},${x.location.country}. ${x.location.postalCode}`)
-               console.log(x.location.timezone)
-               console.log(x.isp)
-               console.log(`${x.location.lat}, ${x.location.lng}` )
-          }
-      });
-   });
+    var j = 0;
+    var api_key = "at_gdyoltZTIMf5uDj0fRHL0HeN0Ovmv";
+
+    //var api_key = "at_gdyoltZTIMf5uDj0fRHL1HeN0Ovmv";
+
+    let api__getter = function(){
+         let ip = document.querySelector("#ip").value;
+             $(function () {
+                if(ValidateIPaddress(ip) === true){
+                    $.ajax({
+                        url: "https://geo.ipify.org/api/v1",
+                        data: {apiKey: api_key, ipAddress: ip},
+                        success: function(data) {
+                            var x = data;
+                            console.log(x)
+                            changeText(x)
+                            lat1 = x.location.lat
+                            lag1 = x.location.lng
+                            changeMap()
+
+                        }
+                    });
+                }else{
+                    alert("You have entered an invalid IP address!")  
+                    return (false)  
+                }
+
+            })
+    } ;
+  
